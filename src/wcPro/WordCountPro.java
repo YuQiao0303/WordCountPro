ac629be36a2765d62be0588f1ab3e15b857c751d
Yu Qiao <691761071@qq.com>
2018-03-31 16:12:23 +0800

    start the project

---
+++ src/wcPro/WordCountPro.java
@@ -0,0 +1,32 @@
+package wcPro;
+import java.util.TreeMap;
+
+public class WordCountPro {
+//---------------------------artributes-----------------------------//
+	TreeMap Info;
+//-----------------------------methods-----------------------------//
+	Boolean IsInputValid(String input){
+		/* 判断参数是否合理，
+		 * 若不合理，报错，return false；(之后主函数退出)
+		 * 若合理，return true（之后主函数中args【0】即为inputfilename）
+		 * 商莹*/
+		return true;
+	}
+
+	void wcPro(String input){
+		
+		/*将单词和词频存入info
+		 * 雷佳谕、余乔
+		 * */
+		
+	} 
+		
+	void output(){    
+		//将Info的相关内容输出到result.txt
+		//蒋雨晨
+	}
+	
+	public static void main(String[] args){
+		//
+	}
+}

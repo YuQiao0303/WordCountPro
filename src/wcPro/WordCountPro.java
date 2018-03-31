40633d8b5f81d80cf49ad15e3c6e2aaf6235a2c3
ShangYing <shangying1998@126.com>
2018-03-31 20:53:10 +0800

    输入控制完成

--- src/wcPro/WordCountPro.java
+++ src/wcPro/WordCountPro.java
@@ -1,16 +1,37 @@
 package wcPro;
+import java.io.File;
 import java.util.TreeMap;
 
 public class WordCountPro {
-//---------------------------artributes-----------------------------//
+//---------------------------Attributes-----------------------------//
 	TreeMap Info;
 //-----------------------------methods-----------------------------//
-	Boolean IsInputValid(String input){
+static Boolean IsInputValid(String[] input){	
+		if(input.length==0){
+			System.out.print("请输入文件名");
+			return false;
+		}
+		else if(input.length>1){
+			System.out.print("请以格式wcPro.exe [input_file_name]输入命令");
+			return false;
+		}
+		else{
+			File file=new File(input[0]);
+			if(!file.exists()){
+				System.out.print("该文件不存在");
+				return false;
+				}
+			else{
+				System.out.print("文件为：");
+				System.out.print(input[0]);
+				return true;
+				}
+			}
 		/* 判断参数是否合理，
 		 * 若不合理，报错，return false；(之后主函数退出)
 		 * 若合理，return true（之后主函数中args【0】即为inputfilename）
 		 * 商莹*/
-		return true;
+	
 	}
 
 	void wcPro(String input){
@@ -27,6 +48,6 @@ public class WordCountPro {
 	}
 	
 	public static void main(String[] args){
-		//
+		IsInputValid(args);
 	}
 }

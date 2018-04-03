5547a4828cf101f4b84ad82f29eac7ae90be473d
leilei <thunderbey@163.com>
2018-04-03 16:17:31 +0800

    test:完成单元测试脚本，并通过所有测试用例

--- src/wcPro/WordCountPro.java
+++ src/wcPro/WordCountPro.java
@@ -1,9 +1,29 @@
 package wcPro;
+import java.util.List;
+
+import java.io.BufferedReader;
+
+import java.io.File;
+
+import java.io.FileReader;
+
+import java.io.IOException;
+
 import java.util.TreeMap;
 
+import java.util.ArrayList;
+
+import java.util.Collections;
+
+import java.util.Map;
+
+import java.util.Map.Entry;
+
+import java.util.Comparator;
+
 public class WordCountPro {
-//---------------------------artributes-----------------------------//
-	TreeMap Info;
+//---------------------------Attributes-----------------------------//
+	static TreeMap<String,Integer> Info = new TreeMap<String,Integer>(); 
 //-----------------------------methods-----------------------------//
 	Boolean IsInputValid(String input){
 		/* 判断参数是否合理，
@@ -12,12 +32,50 @@ public class WordCountPro {
 		 * 商莹*/
 		return true;
 	}
-
-	void wcPro(String input){
+	
+//测试用的方法，返回值为一颗以单词为key，词频为value的红黑树
+static void wcPro(String input) throws IOException{
 		
 		/*将单词和词频存入info
 		 * 雷佳谕、余乔
 		 * */
+		File file=new File(input); 
+		BufferedReader br = new BufferedReader(new FileReader(file)); 
+		String line = null; 
+		//定义一个map集合保存单词和单词出现的个数 
+		//TreeMap<String,Integer> tm = new TreeMap<String,Integer>(); 
+		//读取文件 
+		while((line=br.readLine())!=null){ 
+		   line=line.toLowerCase(); 
+		   String reg1 = "[\\s~`!#%\\^&\\*_\\.\\(\\)\\[\\]\\+=:;\"'\\|<>\\,/\\?0-9]+"; 
+		   String containLetter=".*[a-z].*";
+		   //String reg2 ="\\w+"; 
+		   //将读取的文本进行分割 
+		   String words[] = line.split(reg1); 
+		   for(String word: words){ 
+			   if(word.equals("")||!word.matches(containLetter))
+					   continue;
+			   int firstIndex=0,lastIndex=word.length()-1;
+			   while(word.charAt(firstIndex)=='-')//寻找第一个字母的坐标
+			   {
+				   firstIndex++;
+			   }
+			   while(word.charAt(lastIndex)=='-')//寻找最后一个字母的坐标
+			   {
+				   lastIndex--;
+			   }
+			   word=word.substring(firstIndex, lastIndex+1);
+			   if(!Info.containsKey(word)){ 
+				   Info.put(word,1); 
+			   }
+			   else{ 
+				   Info.put(word,Info.get(word)+1); 
+			   } 
+		   } 
+
+		} 
+
+		br.close();
 		
 	} 
 		
@@ -26,7 +84,10 @@ public class WordCountPro {
 		//蒋雨晨
 	}
 	
-	public static void main(String[] args){
+	public static void main(String[] args) throws IOException{
 		//
+		wcPro("test20.txt");
+		System.out.println(Info);
+		
 	}
 }
